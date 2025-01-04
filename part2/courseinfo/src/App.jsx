5c0a9dbddfe65f480b02a457fb52639b4b1c780f
Hiwersen Angelo Gnocchi Godoy <100746220+hiwersen@users.noreply.github.com>
2025-01-04 16:10:40 +0000

    Implement exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,42 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>
-
-const Total = ({ parts }) => (
-  <b>
-    Total of {
-    parts.reduce((acc, { exercises }) => 
-      acc + exercises
-    , 0)
-    } exercises
-  </b>
-)
-  
-
-const Part = ({ part: { exercises, name } }) => 
-  <p>
-    {name} {exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <div>
-    {
-      parts.map(part =>
-        <Part 
-          key={part.id} 
-          part={part} 
-        />
-      )
-    }    
-  </div>
-
-const Course = ({ course: { name: course, parts } }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
-    </div>
-  )
-}
+import Course from './components/Course.jsx'
 
 const App = () => {
   const courses = [
