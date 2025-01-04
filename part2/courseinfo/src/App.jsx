b25a95d48cdda9e5fa02d72b851e8d7f1314159c
Hiwersen Angelo Gnocchi Godoy <100746220+hiwersen@users.noreply.github.com>
2025-01-04 10:49:42 +0000

    Implement exercise 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,55 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part: { exercises, name } }) => 
+  <p>
+    {name} {exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <div>
+    {
+      parts.map(part =>
+        <Part 
+          key={part.id} 
+          part={part} 
+        />
+      )
+    }    
+  </div>
+
+const Course = ({ course: { name: course, parts } }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
