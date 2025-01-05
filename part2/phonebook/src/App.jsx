1fe52dc7befa2e1cc30b9c21a7f1df61f45de0fa
Hiwersen Angelo Gnocchi Godoy <100746220+hiwersen@users.noreply.github.com>
2025-01-05 10:44:40 +0000

    Implement exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567'
-     }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
 
   const handleChange = setState => 
     event => setState(event.target.value)
@@ -24,18 +25,32 @@ const App = () => {
     } else {
       const person = {
         name: newName,
-        number: newNumber
+        number: newNumber,
+        id: ++persons.length
       }
       setPersons(persons.concat(person))
     }
 
     setNewName('')
     setNewNumber('')
+    setSearch('')
   }
+
+  const personsToDisplay = !search 
+    ? persons
+    : persons.filter(({ name }) => 
+      new RegExp(search, 'i').test(name))
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with 
+          <input 
+            type="text" 
+            value={search}
+            onChange={handleChange(setSearch)}
+          />
+      </div>
       <form onSubmit={handleSubmit}>
         <div>name: 
           <input 
@@ -58,8 +73,8 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         {
-          persons.map(({ name, number }) => 
-            <p key={name}>{name} {number}</p>
+          personsToDisplay.map(({ name, number, id }) => 
+            <p key={id}>{name} {number}</p>
           )
         }
       </div>
